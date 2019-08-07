93396c8ffc46825882a8d08ffacde6352fb68d2a
Julie Park <jspark721@gmail.com>
2019-08-07 15:21:42 -0700

    update landing page hero container

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let homepageLogo = document.getElementById('logo-main');
+let subtitle = document.getElementById('hero-subtitle');
+let line = document.getElementById('hero--line');
+
+function showLogo() {
+  homepageLogo.style.opacity = '1';
+  homepageLogo.style.transform = 'translateY(-20px)';
+}
+
+function showSubtitle() {
+  subtitle.style.opacity = '1';
+  subtitle.style.transform = 'translateX(0)';
+}
+
+function showLine() {
+  line.style.opacity = '1';
+}
+
+setTimeout(showLogo(), 7000);
+setTimeout(showSubtitle(), 22000);
+setTimeout(showLine(), 90000);
