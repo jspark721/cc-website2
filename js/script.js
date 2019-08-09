3d4a9fdcc6cc746d36b42b38c1bad73fc8d7732b
Julie Park <jspark721@gmail.com>
2019-08-09 02:10:11 -0700

    add navigation

--- js/script.js
+++ js/script.js
@@ -21,11 +21,25 @@ setTimeout(showSubtitle(), 22000);
 setTimeout(showLine(), 90000);
 
 
-window.onscroll = function() {
-    let heroSection = document.getElementById('hero');
-    if ( window.pageYOffset > 300 ) {
-        line.classList.add("fixed");
-    } else {
-      return console.log('nothing happened');
-    }
+//menu navbar function
+let body = document.getElementsByTagName("BODY")[0];
+let openNav = document.getElementById("nav--main");
+let menuIcon = document.getElementById("menu--box");
+let closeIcon = document.getElementById("menu--close");
+
+function openMenu() {
+  openNav.style.transform = 'translateY(0)';
+  openNav.style.opacity = '1';
+  body.style.overflow = 'hidden';
+  menuIcon.style.transform = 'translateY(-100px)';
+  closeIcon.style.transform = 'translateY(0)';
+}
+
+function closeMenu() {
+  openNav.style.transform = 'translateY(-100vh)';
+  openNav.style.transition = 'all .5s ease-out';
+  openNav.style.opacity = '0'
+  body.style.overflow = 'visible';
+  menuIcon.style.transform = 'translateY(0)';
+  closeIcon.style.transform = 'translateY(-100px)';
 }
