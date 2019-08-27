e0a24ebb09f857b4affb5d7810047703acde060f
Julie Park <jspark721@gmail.com>
2019-08-27 13:33:59 -0700

    add client page design

--- js/page.js
+++ js/page.js
@@ -1,9 +1,16 @@
 let aboutText = document.getElementById("about-text");
+let clientText = document.getElementById("client-text");
 
 window.onscroll = function() {
   aboutText.style.transform = 'translateY(130px)';
   if(window.scrollY < 5) {
     aboutText.style.transform = 'translateY(0)';
+  }
+}
+window.onscroll = function() {
+  clientText.style.transform = 'translateY(90px)';
+  if(window.scrollY < 5) {
+    clientText.style.transform = 'translateY(0)';
   }
 }
 
