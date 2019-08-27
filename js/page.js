4acf06918ef88a59ac6b18125f8cea1b57dc0d23
Julie Park <jspark721@gmail.com>
2019-08-27 12:53:55 -0700

    update work page

--- js/page.js
+++ js/page.js
@@ -6,3 +6,18 @@ window.onscroll = function() {
     aboutText.style.transform = 'translateY(0)';
   }
 }
+
+//homepage video button
+let workVid = document.getElementById("hero-work-video");
+let btn = document.getElementById("video-btn");
+
+// pause and play the video
+function playPause() {
+  if (workVid.paused) {
+    workVid.play();
+    btn.innerHTML = "pause <i class='fas fa-pause'></i>";
+  } else {
+    workVid.pause();
+    btn.innerHTML = "play <i class='fas fa-play'></i>";
+  }
+}
