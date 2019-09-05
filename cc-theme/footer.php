18bd9c5e09667460fe6a77946938469115c4a715
Julie Park <jspark721@gmail.com>
2019-09-05 15:14:25 -0700

    add cc sub theme for wordpress

---
+++ cc-theme/footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package crispychicken
+ */
+
+?>
+
+
+  <footer>
+    <div class="footer-container">
+      <img class="footer--logo" src="../assets/cc-logo-sm.png" />
+      <p>6060 Center Drive, 10th Floor</p>
+      <p>Los Angeles, CA 90045</p>
+      <ul class="social-icons">
+        <li><a href="mailto:info@crispychicken.co"><i class="far fa-envelope"></i></a></li>
+        <li><a href="https://www.facebook.com/CrispyChickenCo/"  target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="https://www.youtube.com/channel/UCzysAcbGrWTAwHeQ0HHmK_g"  target="_blank"><i class="fab fa-youtube"></i></a></li>
+        <li><a href="https://twitter.com/crispychickenco"  target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="http://instagram.com/crispychickenco"  target="_blank"><i class="fab fa-instagram"></i></a></li>
+      </ul>
+      <p class="sm-cap">© copyright crispy chicken 2019. all rights reserved |
+      web design + development by <a class="a-sm" href="https://julieloves.co">Julie Park</a></p>
+    </div>
+  </footer>
+
+  <script src="../js/navigation.js"></script>
+  <script src="../js/page.js"></script>
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
