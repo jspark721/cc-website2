cc53128fdcb5dc081b4829ea454e978af16ddccb
Julie Park <jspark721@gmail.com>
2019-09-09 14:28:31 -0700

    update contact page on wp theme

--- crispychicken-theme/page-contact.php
+++ crispychicken-theme/page-contact.php
@@ -4,29 +4,31 @@
 get_header();
 ?>
 
-<div class="travelmuse-logo">
-  <a class="logo white" href="/">the travel muse</a>
+<div class="logo">
+  <a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/cc-logo-sm-orange.png" alt="crispy chicken small logo"/></a>
 </div>
-<section class="page--contact">
-  <div class="contact--desc">
-    <h2>Get in Touch</h2>
-    <p>Nathalie would love to chat with you! Please fill out the form below or you can email directly at:</p>
-    <h4>nathaliebasha@yahoo.com</h4>
+
+<section class="page--contact-container">
+  <div class="page--contact-textarea">
+    <div class="page--title">
+      <p class="page--title-sm"><span>contact</span></p>
+      <h1>get in<br>touch<span>.</span></h1>
+      <p>6060 Center Drive, 10th Floor<br>Los Angeles, CA 90045</p>
+    </div>
   </div>
-  <div class="services--line"></div>
-</section>
-<div class="cursive-contact"><h3>contact form</h3></div>
-<div class="contact--container less-height">
-  <?php
-    // Start the loop
-    if(have_posts()) {
-      while(have_posts()) {
-        the_post();
-        the_content();
+  <div class="page--contact-form">
+    <?php
+      // Start the loop
+      if(have_posts()) {
+        while(have_posts()) {
+          the_post();
+          the_content();
+        }
       }
-    }
-  ?>
-</div>
+    ?>
+  </div>
+</section>
+
 
 
 <?php get_footer(); ?>
