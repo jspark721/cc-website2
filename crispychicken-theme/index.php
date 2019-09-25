9d352b001e8f73e917ed0845ebec197662e46008
Julie Park <jspark721@gmail.com>
2019-09-25 16:27:14 -0700

    update client theme page to open up video module

--- crispychicken-theme/index.php
+++ crispychicken-theme/index.php
@@ -20,25 +20,15 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) {
-
-			// Load posts loop.
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( 'template-parts/content/content' );
-			}
-
-			// Previous/next page navigation.
-			twentynineteen_the_posts_navigation();
-
-		} else {
-
-			// If no content, include the "No posts found" template.
-			get_template_part( 'template-parts/content/content', 'none' );
-
-		}
-		?>
+			<?php
+        // Start the loop
+        if(have_posts()) {
+          while(have_posts()) {
+            the_post();
+            the_content();
+          }
+        }
+      ?>
 
 		</main><!-- .site-main -->
 	</section><!-- .content-area -->
--- crispychicken-theme/partials/client-mtv.php
+++ crispychicken-theme/partials/client-mtv.php
@@ -6,58 +6,12 @@
 
 ?>
 
-<section class="client-video-container">
-  <div class="client-video--grid">
-    <div class="client-video--item">
-      <div class="client-video--thumbnail thumbnail-bg">
-        <video autoplay muted loop class="work-video">
-          <source src="<?php echo get_site_url(); ?>/wp-content/uploads/mtv-project1.mp4" type="video/mp4">
-        </video>
-      </div>
-      <div class="client-video--textarea">
-        <div class="client-video-text">
-          <p class="sm-cap"><span>project</span></p>
-          <h3>Jersey Shore Ronnie's Dance Moves</h3>
-        </div>
-        <div class="client--item-overlay">
-          <p>Ideation & Editing</p>
-        </div>
-      </div>
-    </div>
-    <div class="client-video--item">
-      <div class="client-video--thumbnail thumbnail-bg">
-        <video autoplay muted loop class="work-video">
-          <source src="<?php echo get_site_url(); ?>/wp-content/uploads/mtv-project2.mp4" type="video/mp4">
-        </video>
-      </div>
-      <div class="client-video--textarea">
-        <div class="client-video-text">
-          <p class="sm-cap"><span>project</span></p>
-          <h3>The Challenge Spinning Ball</h3>
-        </div>
-        <div class="client--item-overlay">
-          <p>Editing</p>
-        </div>
-      </div>
-    </div>
-    <div class="client-video--item">
-      <div class="client-video--thumbnail thumbnail-bg">
-        <video autoplay muted loop class="work-video">
-          <source src="<?php echo get_site_url(); ?>/wp-content/uploads/mtv-project3.mp4" type="video/mp4">
-        </video>
-      </div>
-      <div class="client-video--textarea">
-        <div class="client-video-text">
-          <p class="sm-cap"><span>project</span></p>
-          <h3>Wild 'N Out TikTok</h3>
-        </div>
-        <div class="client--item-overlay">
-          <p>Ideation & Editing</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+<?php
+  $my_postid = 203;
+  $content = get_post($my_postid)->post_content;
+  echo $content;
+?>
+
 <div class="work-container--main-btn">
   <a href="<?php echo get_site_url(); ?>/work"><button class="btn-light">back to all clients</button></a>
 </div>
